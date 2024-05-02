1ab8a0ab7d399dee573c5618b992cf9cc6f3ea03
ASUS <rikardoperez0809@gmail.com>
2024-05-01 19:59:38 -0500

    Agregar archivos de proyecto.

---
+++ Introduccion/Models/Pais.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Introduccion.Models
+{
+    public class Pais
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [AllowNull]
+
+    }
+}
